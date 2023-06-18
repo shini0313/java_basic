5fa7a09a0d202a9f5e453c4fa3c0b61a9b95e3b9
shini0313 <136160493+shini0313@users.noreply.github.com>
2023-06-18 18:54:47 +0900

    1
    1

---
+++ r0802/Employee.java
@@ -0,0 +1,50 @@
+/**
+ * 質問２︓以下コードの出⼒を回答してください。
+
+// Employee.java
+public class Employee {
+ private String id;
+ public Employee(String id) {
+ this.id = id;
+ }
+ @Override
+ public boolean equals(Object another) {
+ if (another instanceof Employee) {
+ Employee an = (Employee) another;
+ return an.id.equals(id);
+ }
+ return false;
+ }
+}
+// Kicker.java
+public class Kicker {
+ public static void main(String...args) {
+ Employee x = new Employee("1234");
+ Employee y = new Employee("1234");
+ System.out.println(x == y); // true ? false? 原因は ?
+ System.out.println(x.equals(y)); // true ? false? 原因は ?
+ }
+}
+ */
+package r0802;
+
+public class Employee {
+	
+	private String id;
+	public Employee(String id) {
+		
+		this.id =id;
+	}
+	@Override
+	public boolean equals(Object another) {
+		
+		if (another instanceof Employee) {
+			
+			Employee an =(Employee) another;
+			return an.id.equals(id);
+			
+		}
+		return false;
+	}
+
+}
---
+++ r0802/Kicker.java
@@ -0,0 +1,18 @@
+package r0802;
+
+public class Kicker {
+	
+	public static void main(String...args) {
+		
+		Employee x = new Employee("1234");
+		
+		Employee y = new Employee("1234");
+		
+		System.out.println(x==y);
+		//x和y是通过两个不同的new分别创建的，是不同的内存地址，所以false
+		
+		System.out.println(x.equals(y));
+		//里面的结果都是1234，所以是true
+	}
+
+}
