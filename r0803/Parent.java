396c324e0cc7381a15f9e88d3623100237ff0d4d
shini0313 <136160493+shini0313@users.noreply.github.com>
2023-06-18 18:43:05 +0900

    Update Parent.java

--- r0803/Parent.java
+++ r0803/Parent.java
@@ -1,3 +1,31 @@
+/**
+ * 以下コンパイルを修正してください。
+ * // Parent.java
+public class Parent {
+ public void hello() {
+ System.out.println("parent method");
+ }
+}
+// Child.java
+public class Child extends Parent {
+ @Override // 親クラスの強制的にOverrideする意味（アノテーション）
+ public void hello() {
+ System.out.println("child method");
+ }
+}
+// Kicker.java
+public class Kicker {
+ public static void print(Child v) {
+ v.hello();
+ }
+ public static void main(String...args) {
+ print(new Parent());
+  print(new Child());
+ }
+}
+
+
+ */
 package r0803;
 
 public class Parent {
