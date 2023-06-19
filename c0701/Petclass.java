addf8e916c6acdffe9595bece209b3e1bab5e82d
shini0313 <136160493+shini0313@users.noreply.github.com>
2023-06-19 14:08:54 +0900

    1
    1

---
+++ c0701/Petclass.java
@@ -0,0 +1,20 @@
+package c0701;
+
+public class Petclass {
+	
+	public static void main(String[]args) {
+		
+		Pet cat =new Pet("cat",2);
+		
+		Pet dog = new Pet("dog",5);
+		System.out.println(cat.getName());
+		System.out.println(cat.getAge());
+		System.out.println(dog.getName());
+		System.out.println(dog.getAge());
+	}
+	
+		
+
+	
+
+}
